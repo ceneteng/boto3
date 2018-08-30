f4ea15b9c99c5a822a1af0c51f523c6342332fe9
localadmin <eaton.chrisc@gmail.com>
2018-08-30 09:23:27 -0500

    modified to work first time

--- getvols.py
+++ getvols.py
@@ -2,10 +2,9 @@
 
 regions = ['us-west-1','us-west-2','us-east-1']
 import boto3
-import json
 
-ec2 = boto3.client('ec2',region_name='us-east-1')
+for reg in regions:
+	ec2 = boto3.client('ec2',region_name=reg)
 
-for instance in ec2.Instance.all():
-	if ec2.state == stopped:
-		print instance.instance_id
+	for volume in ec2.Volume.all():
+		print(volume.id)
