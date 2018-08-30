524037dc8f4652c3b665a265ca7f528390989bc5
localadmin <eaton.chrisc@gmail.com>
2018-08-30 09:24:23 -0500

    changed client to resource

--- getvols.py
+++ getvols.py
@@ -4,7 +4,7 @@ regions = ['us-west-1','us-west-2','us-east-1']
 import boto3
 
 for reg in regions:
-	ec2 = boto3.client('ec2',region_name=reg)
+	ec2 = boto3.resource('ec2',region_name=reg)
 
 	for volume in ec2.Volume.all():
 		print(volume.id)
