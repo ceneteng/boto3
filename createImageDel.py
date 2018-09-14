071d2bd740d83d603aad76817b5fc2ff1fa00917
localadmin <eaton.chrisc@gmail.com>
2018-09-14 08:57:40 -0500

    problems

--- createImageDel.py
+++ createImageDel.py
@@ -9,7 +9,7 @@ client = boto3.client('ec2', region_name=region)
 vFilter = [{'Name':'tag:Name', 'Values':['*Dev']}]
 vpcs = list(ec2.vpcs.filter(Filters=vFilter))
 vIds = []
-iIds = []
+myIns = []
 instances = ec2.instances.all()
 for vpc in vpcs:
 	
@@ -22,16 +22,18 @@ for instance in instances:
 	if instance.vpc_id in vIds:
 		print instance.tags
 		instance.create_image(Name = instance.instance_id)
-		iIds.append(instance.instance_id)
+		myIns.append(instance.instance_id)
 
 images = list(ec2.image.filter(Filters=[{'Name': 'owner-id', 'Values': ['896917285996']}])
-for i in iIds:
+
+
+for ins in myIns:
 	# ec2.image.wait_until_exists(
 	# 	Filters = [{
 
 	# 		'Name': 'name',
 	# 		'Values': [
-	# 			i, 
+	# 			ins, 
 
 	# 		]
 	# 	},
